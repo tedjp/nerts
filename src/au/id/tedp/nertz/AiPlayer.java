d7b2b809dce69eac985449cd7f634adb07ca6914
Ted Percival <ted@tedp.id.au>
2012-02-03 13:50:30 -0700

    Move makeMove function into Player
    From AiPlayer, so that the Human can play automatically (demo mode/test
    mode).

--- src/au/id/tedp/nertz/AiPlayer.java
+++ src/au/id/tedp/nertz/AiPlayer.java
@@ -15,25 +15,4 @@ class AiPlayer extends Player {
         super("CPU", lake, state);
         this.game = game;
     }
-
-    public void makeMove(GameMove move) {
-        if (move == null)
-            return;
-
-        try {
-            if (move != null) {
-                Log.d("Nertz", "Executing move: " + move.toString());
-                move.execute();
-            }
-
-            // This should really be in the (move != null) condition,
-            // but putting it here makes it more visible by logging it
-            // on every turn.
-            if (getNertzPile().isEmpty())
-                Log.d("Nertz", "AI Player " + this + " calls Nertz!");
-        }
-        catch (InvalidMoveException e) {
-            Log.e("Nertz", "Failed to play AI move: " + e.getMessage());
-        }
-    }
 }
--- src/au/id/tedp/nertz/Player.java
+++ src/au/id/tedp/nertz/Player.java
@@ -134,4 +134,25 @@ class Player {
 
         return null;
     }
+
+    public void makeMove(GameMove move) {
+        if (move == null)
+            return;
+
+        try {
+            if (move != null) {
+                Log.d("Nertz", "Executing move: " + move.toString());
+                move.execute();
+            }
+
+            // This should really be in the (move != null) condition,
+            // but putting it here makes it more visible by logging it
+            // on every turn.
+            if (getNertzPile().isEmpty())
+                Log.d("Nertz", "AI Player " + this + " calls Nertz!");
+        }
+        catch (InvalidMoveException e) {
+            Log.e("Nertz", "Failed to play AI move: " + e.getMessage());
+        }
+    }
 }
